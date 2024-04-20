62599e514e5b1c17f605ef62f0faaaafa604292f
ronaldo.farrapo <ronaldo.farrapo@mprj.mp.br>
2024-04-20 20:33:48 -0300

    feat: Tratando response errors de maneira global.

---
+++ src/main/java/med/voll/api/infra/GlobalControllerExceptionHandler.java
@@ -0,0 +1,33 @@
+package med.voll.api.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@RestControllerAdvice
+public class GlobalControllerExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity notFound(){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<DadosErroValidacao>> badRequest(MethodArgumentNotValidException ex){
+        var errors = ex.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(errors.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    private record DadosErroValidacao(String field, String message){
+        public DadosErroValidacao(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+    }
+
+}
